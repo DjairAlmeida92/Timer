3387c3322a53d796b141d7ee4cf3d56f19b019fc
djair <djaptbr10@gmail.com>
2022-10-05 15:08:20 +0100

    meu timer

---
+++ script.js
@@ -0,0 +1,32 @@
+// 1º criar uma função com dois argumentos
+
+function startTimer(duration, display) {
+
+    var timer = duration, minutes, seconds;
+
+    setInterval (function() {
+
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if(--timer < 0) {
+            timer = duration;
+        }
+
+    }, 1000);
+
+}
+
+window.onload = function() {
+
+    var duration = 60 * 10; // Conversão para segundos
+    var display = document.querySelector("#timer"); // Elemento para exibir o timer
+
+    startTimer(duration, display); // Inicia a função
+    
+}
